e7d574c588939945d10d8415e573bc1f5b8abc4d
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-18 09:22:58 +0530

    [Tejas] add Multiple contact details added

--- AddressBook.cs
+++ AddressBook.cs
@@ -21,7 +21,23 @@ namespace AddressBookProject
 
             addressBook.Add(contact.FirstName, contact);
         }
+        public void ViewData(string name)
+        {
+            foreach (KeyValuePair<string, Contacts> i in addressBook)
+            {
+                if (i.Key.Equals(name))
+                {
+                    Console.WriteLine("First name is " + i.Value.FirstName);
+                    Console.WriteLine("Last name is " + i.Value.LastName);
+                    Console.WriteLine("Address is " + i.Value.Address);
+                    Console.WriteLine("Phone number is " + i.Value.PhoneNuber);
+                    Console.WriteLine("State is " + i.Value.State);
+                    Console.WriteLine("City is " + i.Value.City);
+                    Console.WriteLine("Email ID is " + i.Value.Email);
+                }
 
+            }
+        }
         public void ViewData()
         {
             foreach(KeyValuePair<string, Contacts> i in addressBook)
--- IContacts.cs
+++ IContacts.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace AddressBookProject
 {
@@ -8,6 +9,7 @@ namespace AddressBookProject
     {
         public void AddContact(string fname, string lname, string address, string phone, string state, string city, string email);
         public void EditContact(string name);
+        public void ViewData();
 
         public void DeleteContact(string name);
     }
