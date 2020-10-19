dd44bcfbdcb93405bc3cf8db4e1c5229c584c06e
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-19 08:38:18 +0530

    [tejas] add Search by name enabled

--- AddBook.cs
+++ AddBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace AddressBookProject
 {
-    public class AddBook
+    public class AddBook 
     {
         Dictionary<string, AddressBook> collectionAddressBook = new Dictionary<string, AddressBook>();
         public void AddAddressBook(string firstName)
@@ -21,14 +21,53 @@ namespace AddressBookProject
             }
             return null;
         }
-        public bool CheckDuplicateName (string firstName)
+        public bool CheckDuplicateName(string firstName)
         {
-            if(collectionAddressBook.ContainsKey(firstName))
+
+            if (collectionAddressBook.ContainsKey(firstName))
             {
                 return false;
             }
             return true;
         }
-    }
+        public string[] SearchByCity(string searchKey)
+        {
+            int countOfList = 0;
+            string[] listOfSearchPersonsByCity = new string[] { };
+            Dictionary<string, AddressBook>.Enumerator enumerator = collectionAddressBook.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                AddressBook searchAddressBook = enumerator.Current.Value;
+                List<Contacts> listOfSearch = searchAddressBook.GetAddressBook();
+                foreach (Contacts i in listOfSearch)
+                {
+                    if (i.City == searchKey)
+                    {
+                        listOfSearchPersonsByCity[countOfList++] = i.FirstName;
+                    }
+                }
+                return listOfSearchPersonsByCity;
+            }
+             string[] SearchByState(string searchKey)
+            {
+                int countOfList = 0;
+                string[] listOfSearchPersonsByState = new string[] { };
+                Dictionary<string, AddressBook>.Enumerator enumerator = collectionAddressBook.GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    AddressBook searchAddressBook = enumerator.Current.Value;
+                    List<Contacts> listOfSearch = searchAddressBook.GetAddressBook();
+                    foreach (Contacts i in listOfSearch)
+                    {
+                        if (i.City == searchKey)
+                        {
+                            listOfSearchPersonsByState[countOfList++] = i.FirstName;
+                        }
+                    }
+                }
+                return listOfSearchPersonsByState;
+            }
 
-}
+        } } }
+
+
